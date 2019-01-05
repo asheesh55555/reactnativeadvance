1999e36edca3a3a15cd9a7d3ff7e532825de0fdb
rakesh maurya <rakeshmaurya262@gmail.com>
2019-01-05 13:44:01 +0530

    dishesh grid view

--- DisplaydishesScreen.js
+++ DisplaydishesScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {View, Text, ScrollView, Image } from 'react-native';
 import { ListItem } from 'react-native-elements';
@@ -6,12 +6,6 @@ import { ListItem } from 'react-native-elements';
 
 export default class DisplaydishesScreen extends React.Component {
   render() {
-   /* return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>dishes Screen {this.props.navigation.state.params.dishes[0].name}</Text>
-      </View>
-    );*/
-
     return(
       <ScrollView>
       {
@@ -32,4 +26,68 @@ export default class DisplaydishesScreen extends React.Component {
     </ScrollView>
     );
   }
+}*/
+
+
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import GridView from 'react-native-super-grid';
+
+export default class DisplaydishesScreen extends Component {
+  render() {
+    // Taken from https://flatuicolors.com/
+    // const items = [
+    //   { name: 'TURQUOISE', code: '#1abc9c' }, { name: 'EMERALD', code: '#2ecc71' },
+    //   { name: 'PETER RIVER', code: '#3498db' }, { name: 'AMETHYST', code: '#9b59b6' },
+    //   { name: 'WET ASPHALT', code: '#34495e' }, { name: 'GREEN SEA', code: '#16a085' },
+    //   { name: 'NEPHRITIS', code: '#27ae60' }, { name: 'BELIZE HOLE', code: '#2980b9' },
+    //   { name: 'WISTERIA', code: '#8e44ad' }, { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
+    //   { name: 'SUN FLOWER', code: '#f1c40f' }, { name: 'CARROT', code: '#e67e22' },
+    //   { name: 'ALIZARIN', code: '#e74c3c' }, { name: 'CLOUDS', code: '#ecf0f1' },
+    //   { name: 'CONCRETE', code: '#95a5a6' }, { name: 'ORANGE', code: '#f39c12' },
+    //   { name: 'PUMPKIN', code: '#d35400' }, { name: 'POMEGRANATE', code: '#c0392b' },
+    //   { name: 'SILVER', code: '#bdc3c7' }, { name: 'ASBESTOS', code: '#7f8c8d' },
+    // ];
+
+    return (
+      <GridView
+        itemDimension={130}
+        items={this.props.navigation.state.params.dishes}
+        style={styles.gridView}
+        renderItem={item => (
+          <View style={[styles.itemContainer, { backgroundColor: '#3498db' }]}>
+            <Image style={{width: 50, height: 50}}
+		          source={{uri: item.image}}
+		        />
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemCode}>{item.desc}</Text>
+          </View>
+        )}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  gridView: {
+    paddingTop: 25,
+    flex: 1,
+  },
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
+  },
+});
